0f71572f3db3ad4a684e2372cfbefe4e53683dea
SL <sangwe13@gmail.com>
2025-04-08 19:53:15 -0700

    Initialize with latest
    update repo with latest code

---
+++ Source/EditorUnreal/EditorUnreal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EditorUnreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EditorUnreal, "EditorUnreal" );
